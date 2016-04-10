3186b5467bc2a409c0487389919a8aaebbf2ccbd
Vishv19 <vishvbrahmbhatt19@gmail.com>
2016-04-09 19:48:06 -0700

    Making a sample nodejs with get and post call

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+
+app.get('/allItems', function (req, res) {
+   console.log( "Got a request for allItems" );
+   var data = {
+        "items":[
+        {
+            "id": 1,
+            "name": "A green t-shirt",
+            "price": 12.50,
+            "tags": ["t-shirt", "green"]            
+        },
+        {
+            "id": 2,
+            "name": "A blue jeans",
+            "price": 12.50,
+            "tags": ["jeans", "blue"]
+        }
+
+        ]
+    };
+   res.end( JSON.stringify(data) );
+});
+
+app.post('/item', function (req, res) {
+   console.log( "Got a request for allItems" );
+   var data = {
+        "Message" : "Data Saved Successfully",
+        "id": 1,
+        "name": "A green t-shirt",
+        "price": 12.50,
+        "tags": ["t-shirt", "green"]
+    };
+   res.end( JSON.stringify(data) );
+});
+
+// Start the server
+app.set('port', process.env.PORT || 3000);
+ 
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
