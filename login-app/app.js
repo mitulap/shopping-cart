188f76f850b65236201ab91bbe76046c4ba3894c
mitulap <mitulpatel.hsd@gmail.com>
2016-04-16 07:56:59 +0100

    adding details to app.js

--- login-app/app.js
+++ login-app/app.js
@@ -21,30 +21,65 @@ app.use(expressSesssion({
 	saveUninitialized: false
 }));
 
-
-
 app.use(passport.initialize());
 app.use(passport.session());
 
+
+//verify username and password is correct
+passport.use(new passportLocal.Strategy(function(username, passowrd, done){
+	
+	console.log("Coming here....");
+
+	//will check from data base for authentication
+
+	//as of now I am doing normal authentication and pretending that this is using real database
+	if(username === passowrd){
+		done(null, {id: username, name: username});
+	} else {
+		done(null, null);
+	}
+
+
+	//done(null, user);//if user is authenticated then we will pass user object and null error
+	//done(null, null);//if user is not authenticated then we will pass null object and null error 
+	//done(new Error('ouch!'));
+
+}));
+
+
+passport.serializeUser(function(user, done){
+	done(user.id);
+});
+
+
+passport.deserializeUser(function(id, done){
+	//query database or cache
+	done({id: id, name: id});
+
+});
+
 app.get('/', function(req, res){
 	res.render('index', {
-		isAuthenticated: false,
+		isAuthenticated: req.isAuthenticated(),
 		user: req.user
 	});
 });
 
 
 app.get('/login', function(req, res){
+	
 	res.render('login');
 });
 
-app.get('/post', function(req, res){
-		
+
+//call where we are authenticating user
+app.post('/login', passport.authenticate('local') , function(req, res){
+	console.log("Coming here....");
+	res.redirect('/');
 });
 
 var port  = process.env.port || 1337;
 
 app.listen(port, function(){
-
 	console.log('http://127.0.0.1:' + port + '/' );
 });
