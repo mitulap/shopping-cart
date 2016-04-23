aaaca6e6abaecd4a2d6d8209548e215af971a762
Vishv19 <vishvbrahmbhatt19@gmail.com>
2016-04-22 20:51:14 -0700

    Removing old deprecated api calls

--- app.js
+++ app.js
@@ -6,37 +6,4 @@ app.use(bodyParser.json());
 require('./app/routes/product')(app);
 require('./app/routes/user')(app);
 
-app.get('/allItems', function (req, res) {
-   console.log( "Got a request for allItems" );
-   var data = {
-        "items":[
-        {
-            "id": 1,
-            "name": "A green t-shirt",
-            "price": 12.50,
-            "tags": ["t-shirt", "green"]            
-        },
-        {
-            "id": 2,
-            "name": "A blue jeans",
-            "price": 12.50,
-            "tags": ["jeans", "blue"]
-        }
-        ]
-    };
-   res.end( JSON.stringify(data) );
-});
-
-app.post('/item', function (req, res) {
-   console.log( "Got a post request for creating a new item" );
-   var data = {
-        "Message" : "Data Saved Successfully",
-        "id": 1,
-        "name": "A green t-shirt",
-        "price": 12.50,
-        "tags": ["t-shirt", "green"]
-    };
-   res.end( JSON.stringify(data) );
-});
-
 module.exports = app;
