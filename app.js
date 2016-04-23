157e8fe624f3510ad288581c5795bc605e345448
Vishv19 <vishvbrahmbhatt19@gmail.com>
2016-04-22 23:52:56 -0700

    Committing changes to bodyparser in application

--- app.js
+++ app.js
@@ -6,18 +6,16 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/ShopCart');
 
-app.use(bodyParser.json());
-
-require('./app/routes/product')(app);
-require('./app/routes/user')(app);
-
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+require('./app/routes/product')(app);
+require('./app/routes/user')(app);
+
 app.use('/', routes);
-app.use('/users', users);
+// app.use('/users', users);
 
 module.exports = app;
