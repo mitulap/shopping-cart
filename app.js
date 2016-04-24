99e8204de5f493defb32691532ef2900130663b4
hemilcp <hemilcp@users.noreplay.github.com>
2016-04-23 17:54:56 -0700

    Adding support for express session in routes

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var users = require('./routes/users');
 var mongoose = require('mongoose');
+var session = require('express-session');
+var redisStore = require("connect-redis")(session);
+var redis = require('redis');
+var redisClient = redis.createClient(6379, "localhost");
+
 
 mongoose.connect('mongodb://localhost/ShopCart');
 
-app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+	key : 'redis key',
+    secret: 'ssshhhhh',
+    // create new redis store.
+    store: new redisStore({ host: 'localhost', port: 6379, client: redisClient,ttl :  260}),
+    saveUninitialized: false,
+    resave: false
+}));
+
 
 require('./app/routes/product')(app);
 require('./app/routes/user')(app);
-
-app.use('/', routes);
-// app.use('/users', users);
 
 module.exports = app;
