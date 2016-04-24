7ceecc831be0a57d8cc843e5cd15025680238f52
hemilcp <hemilcp@users.noreplay.github.com>
2016-04-23 17:36:11 -0700

    Modifying the Mongoose model for User Schema

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -6,7 +6,8 @@ var schema = new Schema({
 	username : { type: String, required : true, unique : true},
 	password : { type : String, required : true},
 	email : {type : String, required : true },
-	phone : { type : String}
+	phone : { type : String},
+	address : {type : String }
 
 }, { strict : false});
 
