b1c8bca99afbe615d7484b2c44726db027386c96
hemilcp <hemilcp@users.noreplay.github.com>
2016-04-25 17:41:33 -0700

    Adding the logic to generate Unique User-id upon SignUp

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
-
+var uuid = require('node-uuid');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
+	user_id : { type : String, default : function(){ return uuid(); } },
 	username : { type: String, required : true, unique : true},
 	password : { type : String, required : true},
 	email : {type : String, required : true },
