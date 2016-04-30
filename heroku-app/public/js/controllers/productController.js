1c7d18d8aaa22a4b556cefc36d45a66219a17939
mitulap <mitulpatel.hsd@gmail.com>
2016-05-01 00:19:49 +0100

    Adding shopping cart structure with a simple function with out css

--- heroku-app/public/js/controllers/productController.js
+++ heroku-app/public/js/controllers/productController.js
@@ -1,9 +1,12 @@
 shoppingCart.controller('ProductController', [ '$scope', '$interval', '$http', '$location' ,'$cookieStore' ,function($scope, $interval, $http, $location, $cookieStore) {
-
+    $scope.errorProduct = false;
 	if($cookieStore.get('isAuth') != true){
         $location.path( "/login" );
     }
 
+
+    counter = 0;
+    $scope.productList = true;
     $scope.searchProduct = function(){
 
     	var urlPrefix = "http://open.api.ebay.com/shopping?callbackname=JSON_CALLBACK&callname=FindPopularItems&responseencoding=JSON&appid=HemilPar-Shopping-PRD-a2f839365-478878d5&siteid=0&QueryKeywords=";
@@ -14,45 +17,42 @@ shoppingCart.controller('ProductController', [ '$scope', '$interval', '$http', '
 
     	$http({
             method: 'JSONP',
-            url: url,
-            jsonCallback: "myfun"
-            //params : {callback : 'JSON_CALLBACK'}
+            url: url
         }).success(function (data) {
             //$scope.restaurants = data;
-            $scope.products = data.ItemArray.Item;
-
             console.log(data);
+            $scope.errorProduct = false;
+            if(data.Ack == "Failure"){
+                $scope.products = null;
+                $scope.productList = false;
+                $scope.errorProduct = true;
+            }else{
+                $scope.productList = true;
+                $scope.errorProduct = false;
+                $scope.products = data.ItemArray.Item;
+            }
             
         }).error(function(err){
             console.log(err);
             /*$location.path("/" + "login");*/
         });
 
+    };
 
-        $scope.shortenProductName = function(pName){
+    $scope.shortenProductName = function(pName){
 
-            pName = substr(pName,0,50);
-            if(pName.length > 50){
-                return pName + "...";
-            }else{
-                return pName;
-            }
-        };
+        pName = substr(pName,0,50);
+        if(pName.length > 50){
+            return pName + "...";
+        }else{
+            return pName;
+        }
+    };
 
 
-        /*$.ajax({
-            type: 'GET',
-            dataType: "json",
-            crossDomain: true,
-            url: url,
-            success: function (responseData, textStatus, jqXHR) {
-               console.log("Yay!!!");
-            },
-            error: function (responseData, textStatus, errorThrown) {
-                console.log(responseData);
-                console.log("something went wrong!! Error: "+textStatus);
-            }
-        });*/
+    $scope.addProduct = function() {
 
-    }
+        $scope.totalProductsInCart = ++counter;
+
+    };
 }]);
