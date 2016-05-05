f268af5966633c480c66a60b07df05492868dd7c
Vishv19 <vishvbrahmbhatt19@gmail.com>
2016-05-04 22:38:43 -0700

    Logic to allow cross origin request in express

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ mongoose.connect('mongodb://localhost/ShopCart');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.all(expressJWT({ secret: 'Ebay Shopping cart'}). unless({ path: ['/users/login']}));
 
 //Modified express later on
