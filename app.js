9ef12e202440e198137f26855660873c5726ff8c
Vishv19 <vishvbrahmbhatt19@gmail.com>
2016-05-05 22:24:39 -0700

    Allow token header for apis

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
   next();
 });
 app.all(expressJWT({ secret: 'Ebay Shopping cart'}). unless({ path: ['/users/login']}));
