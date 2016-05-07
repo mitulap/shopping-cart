0c376ba65de2cf64c79469119735554e46fa93e0
Vishv19 <vishvbrahmbhatt19@gmail.com>
2016-05-06 21:12:26 -0700

    Handling quantity for products and returning count from cart

--- app/routes/product.js
+++ app/routes/product.js
@@ -5,15 +5,17 @@ It handles the request of (1) Creating a new product.
 */
 
 /*
-CREATE TABLE product(productname text, productprice float, productid text, productcategory text, productimageurl text, userid text, PRIMARY KEY((userid), productid));
+CREATE TABLE product(productname text, productprice float, productid text, productcategory text, productimageurl text, userid text, quantity int, PRIMARY KEY((userid), productid));
 INSERT INTO product(productname, productprice, productid, productcategory, productimageurl, userid ) VALUES ('mobile', 15.2, '1234', 'electronics', 'http://example.com/product', '123hash');
 */
 
 const cassandra = require('cassandra-driver');
 const client=new cassandra.Client({contactPoints : ['localhost:9042']});
 var getAllProductsOfUser = 'SELECT * FROM products.product WHERE userid=?';
-var createProductOfUser = 'INSERT INTO products.product(productname, productprice, productid, productcategory, productimageurl, userid) VALUES(?, ?, ?, ?, ?, ?)';
-var deleteProductOfUser = 'DELETE FROM products.product WHERE userid=? AND productid=? AND productname=?'
+var createProductOfUser = 'INSERT INTO products.product(productname, productprice, productid, productcategory, productimageurl, userid, quantity) VALUES(?, ?, ?, ?, ?, ?, ?)';
+var deleteProductOfUser = 'DELETE FROM products.product WHERE userid=? AND productid=?'
+var getParticularProductOfUser = 'SELECT * FROM products.product WHERE userid=? AND productid=?'
+var updateParticularProductOfUser = 'UPDATE products.product SET quantity=? WHERE userid=? AND productid=?';
 var redisClient = require('../routes/redisConn');
 var errorResponse = require('./errorResponse');
 
@@ -28,18 +30,34 @@ module.exports = function(app) {
         var productImageUrl = body.product.imageurl;
         var userid = req.params.userid;
         var token = req.get('token');
-        var values = [productName, productPrice, productId, productCategory, productImageUrl, userid];
-
+        var intialValues = [productName, productPrice, productId, productCategory, productImageUrl, userid, 1];
 
         if(token){
             redisClient.get(userid, function(err, reply){
                 if(reply === token) {
-                    client.execute(createProductOfUser,values, { prepare: true }, function(err, result) {
-                        if(err) {
-                            res.status(404).send({msg: err});
+                    client.execute(getParticularProductOfUser,[userid, productId],{ prepare: true }, function(err, getresult){
+                        if(getresult.rows.length === 0) {
+                            client.execute(createProductOfUser,values, { prepare: true }, function(err, result) {
+                                if(err) {
+                                    res.status(404).send({msg: err});
+                                }
+                                else {
+                                    res.status(201).json({productname:productName, productprice: productPrice, productid: productId, productcategory:productCategory, productimageurl: productImageUrl, userid:userid, quantity:1, saved:'true'});
+                                }
+                            });
                         }
                         else {
-                            res.status(201).json({productname:productName, productprice: productPrice, productid: productId, productcategory:productCategory, productimageurl: productImageUrl, userid:userid, saved:'true'});
+                            var itemquantity = getresult.rows[0].quantity;
+                            var updateitemquantity = itemquantity + 1;
+                            var updatedvalues = [updateitemquantity, userid, productId];
+                            client.execute(updateParticularProductOfUser,updatedvalues, { prepare: true }, function(err, result) {
+                                if(err) {
+                                    res.status(404).send({msg: err});
+                                }
+                                else {
+                                    res.status(201).json({productname:productName, productprice: productPrice, productid: productId, productcategory:productCategory, productimageurl: productImageUrl, userid:userid, quantity:updateitemquantity, saved:'true'});
+                                }
+                            });                            
                         }
                     });
                 }
@@ -81,26 +99,53 @@ module.exports = function(app) {
 
     });
 
-    app.put('/products/:productName', function(req, res) {
-        return res.json({name:'XYZ', price:'123', category: 'dummy'});
-    });
-
-    app.delete('/products/:userid/:productid/:productname', function(req, res) {
-        var body = req.body;
+    app.get('/products/:userid/count', function(req, res) {
         var userid = req.params.userid;
-        var productid = req.params.productid;
-        var productname = req.params.productname;
         var token = req.get('token');
 
         if(token){
             redisClient.get(userid, function(err, reply){
                 if(reply === token) {
-                    client.execute(deleteProductOfUser, [userid, productid, productname], { prepare: true }, function(err, result) {
+                    client.execute(getAllProductsOfUser,[userid],{ prepare: true }, function(err, result){
+                        if(err){
+                            res.status(404).send({msg: err});
+                        }
+                        else {
+                            res.status(200).json({ count: result.rows.length});
+                        }
+                    });
+                }
+                else {
+                    return res.status(401).json(errorResponse('Invalid Query!', 401));
+                }
+            });
+        }
+
+        else {
+            return res.status(401).json(errorResponse('Invalid Query!', 401));
+        }
+
+    });
+
+    app.put('/products/:productName', function(req, res) {
+        return res.json({name:'XYZ', price:'123', category: 'dummy'});
+    });
+
+    app.delete('/products/:userid/:productid', function(req, res) {
+        var body = req.body;
+        var userid = req.params.userid;
+        var productid = req.params.productid;
+        var token = req.get('token');
+
+        if(token){
+            redisClient.get(userid, function(err, reply){
+                if(reply === token) {
+                    client.execute(deleteProductOfUser, [userid, productid], { prepare: true }, function(err, result) {
                         if(err){
                             res.status(404).json(errorResponse(err, 404));
                         }
                         else {
-                            res.status(200).json(result);
+                            res.status(200).json({delete:"true"});
                         }           
                     });
                 }
